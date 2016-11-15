741ee1316e45015b9cb34e4fdbd2d5cea59ca6bc
David Kramer <dkramer@student.neumont.edu>
2016-11-15 09:58:07 -0700

    Removed the non-express orm stuff

--- app.js
+++ app.js
@@ -7,31 +7,6 @@ var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 var session = require('express-session');
 
-// Orm Stuff -- Required for Project
-//orm.connect("mysql://neumont:sugarc0deit@mysql.hlaingfahim.com/donutsprinkles", function(err, db) {
-//    if (err) throw err;
-//
-//    // User Model;
-//    var User = db.define("Users", {
-//        id: Number,
-//        username: String,
-//        password: String,
-//    });
-//
-//    // Add table to the database
-//    db.sync(function (err) {
-//        if (err) throw err;
-//
-//        User.create({id: 101, username: "Test", password: "goatCheese"}, function (err) {
-//            if (err) throw err;
-//
-//            User.find({username: "Test"}, function (err, people) {
-//                if (err) throw err;
-//                });
-//            });
-//        });
-//});
-
 // mysql database connection
 var mysql = require('mysql');
 var app = express();
@@ -46,7 +21,7 @@ app.use(orm.express("mysql://neumont:sugarc0deit@mysql.hlaingfahim.com/donutspri
             username: String,
             password: String,
         });
-            next();
+        next();
     }
 }));
 
@@ -65,7 +40,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -84,7 +59,7 @@ app.use(session({
     saveUninitialized: true
 }));
 
-var auth = function(req, res, next) {
+var auth = function (req, res, next) {
     if (req.session && req.session.user != null) {
         return next();
     }
@@ -94,13 +69,13 @@ var auth = function(req, res, next) {
 };
 
 // login
-app.post('/login', function(req, res) {
-	// check to make sure user entered in values
-	if (validateLoginForm(req, res)) {
+app.post('/login', function (req, res) {
+    // check to make sure user entered in values
+    if (validateLoginForm(req, res)) {
         var username = req.body.username;
         var password = req.body.password;
 
-        req.models.user.find({ username: username, password: password }, function (err, user) {
+        req.models.user.find({username: username, password: password}, function (err, user) {
             if (err) {
                 res.send("SQL Error: " + err.message);
                 return;
@@ -108,15 +83,15 @@ app.post('/login', function(req, res) {
 
             if (user.length == 1) {
                 req.session.user = user[0];
-                res.render("profile", { user: user.username});
+                res.render("profile", {user: user.username});
             } else {
                 res.render("loginFailure");
             }
         });
-	}
+    }
 });
 
-app.get('/logout', function(req, res) {
+app.get('/logout', function (req, res) {
     if (req.session.user) {
         req.session.destroy();
         res.render("logout");
@@ -126,7 +101,7 @@ app.get('/logout', function(req, res) {
     }
 });
 
-app.get('/profile', auth, function(req, res) {
+app.get('/profile', auth, function (req, res) {
     res.render("profile");
 });
 
@@ -143,7 +118,7 @@ function validateLoginForm(req, res) {
     // echo back form with errors displayed
     var errors = req.validationErrors();
     if (errors) {
-        res.render("login", { username : username, password : password, errors: errors });
+        res.render("login", {username: username, password: password, errors: errors});
         return false;
     }
     return true;
@@ -154,10 +129,10 @@ function createDBConnection() {
 
     // connection settings
     var connection = mysql.createConnection({
-        host     : 'mysql.hlaingfahim.com',
-        user     : 'neumont',
-        password : 'sugarc0deit',
-        database : 'donutsprinkles'
+        host: 'mysql.hlaingfahim.com',
+        user: 'neumont',
+        password: 'sugarc0deit',
+        database: 'donutsprinkles'
     });
 
     // connect to database
@@ -170,21 +145,21 @@ function createDBConnection() {
 }
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
