6a8f498e965ad097b764164a2a3c6454a9bffa3b
David Kramer <dkramer@student.neumont.edu>
2016-11-30 17:52:10 -0700

    Fixed some style issues and canvas text layout

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -5,8 +5,20 @@ function main() {
     ctx = statsCanvas.getContext("2d");
         ctx.font = "30px Antic Slab";
         ctx.fillStyle = "#4E8E8D";
-        ctx.fillText("Words Per Minute",27,100);
-        ctx.fillText("Accuracy",85,250);
+
+		drawText("Highest WPM", 150);
+		drawText("Average WPM", 300);
+}
+
+function drawText(text, y) {
+	var textWidth = ctx.measureText(text).width;
+	var centerX = getCenterX(textWidth);
+	ctx.fillText(text, centerX, y);
+}
+
+function getCenterX(width) {
+	var centerX = (statsCanvas.width - width) / 2;
+	return centerX;
 }
 
 var interval = setInterval(function(){
